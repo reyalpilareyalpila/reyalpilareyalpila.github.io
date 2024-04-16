720c2720b781abf0d3c1a2f4c4717c472152bd51
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-04-17 04:51:22 +0800

    Update aliplayer.js

--- aliplayer.js
+++ aliplayer.js
@@ -1,1 +1,9 @@
-console.log("666")
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?46c6287ce998a2d843c4a07c9f549efa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
