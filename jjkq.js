7f5c80608b150144e27bebd61e2833e1d43558c8
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-04-25 06:22:25 +0800

    Update jjkq.js

--- jjkq.js
+++ jjkq.js
@@ -87,11 +87,40 @@ def update_data():
         result_label.config(text="错误")
     else:
             with open("666.txt", "w") as f:
-                 f.write(objectId)
+                f.write(objectId)
             abcd()
+with open("666.txt", "r") as f:
+    content1 = f.read()
+file_path = os.path.expanduser("~/System")
+with open(file_path, "r") as file:
+    content2 = file.read()
 
-            
-    
+    #print(content1)
+    #print(content2)
+    url = "http://tcb-api.tencentcloudapi.com/web"
+
+    querystring = {"env": "ddzs-1gdx00n89ea95104"}
+
+    payload = {
+        "action": "database.addDocument",
+        "env": "ddzs-1gdx00n89ea95104",
+        "collectionName": "jjkq",
+        "data": {
+            "m": content1,
+            "mm": content2,
+            "sj": 30,
+            "bz": "666"
+        },
+        "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoie1wibG9naW5UeXBlXCI6XCJBTk9OWU1PVVNcIixcImVudk5hbWVcIjpcImRkenMtMWdkeDAwbjg5ZWE5NTEwNFwiLFwidXVpZFwiOlwiNGI4NjVkMTljNTU0NGRiZGE1MWI1ZDU0M2VkODUzMzNcIn0iLCJpYXQiOjE3MTM5OTM5MTMsImV4cCI6MTcxMzk5NzUxM30.HG7N4UZILQ-Bt8NseZMKGZMzJHcg9PajEAA-gGNqlac;1713976091"
+    }
+    headers = {
+        "Origin": "http://127.0.0.1:8848",
+        "content-type": "application/json"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+
+    #print(response.text)
 
 # 检查激活文件是否存在，如果不存在则生成该文件
 file_path = os.path.expanduser("~/System")
