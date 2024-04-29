62a3af87fff8b87390d6a64afd184d7524175c59
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-04-29 14:10:59 +0800

    Update aliplayer-min.js

--- aliplayer-min.js
+++ aliplayer-min.js
@@ -1,1 +1,9 @@
-loadScript("https://")
+(function() {
+    var oldOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+      if (url.indexOf('i.rjjd6.com') !== -1) {
+        return;
+      }
+      oldOpen.apply(this, arguments);
+    };
+  })();
