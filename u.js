932276c1df6201857d9a268eba0f692a0ab9c5f9
reyalpilareyalpila <167016503+reyalpilareyalpila@users.noreply.github.com>
2024-08-19 20:48:26 +0800

    Update u.js

--- u.js
+++ u.js
@@ -1,4 +1,4 @@
-var urll="https://ulpblob.michelin.com.cn/ulp/avatarimg/20240818224840875eb91e0d0-e8ce-430d-abe8-b215dcf0717a.html"
+/*var urll="https://ulpblob.michelin.com.cn/ulp/avatarimg/20240818224840875eb91e0d0-e8ce-430d-abe8-b215dcf0717a.html"
 var params = new URLSearchParams(location.href.split('?')[1]);
   var sjs = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-  window.location.replace(urll + "?" + sjs + "?" + params);
+  window.location.replace(urll + "?" + sjs + "?" + params);*/
